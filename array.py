76eab2365e7d3b4fa7a51207cc1b7e680a39692f
NVLuat <129598838+mrlaw74@users.noreply.github.com>
2025-12-27 07:25:54 +0000

    Selection sort

--- array.py
+++ array.py
@@ -40,3 +40,32 @@ for i in range (n-1):
     if sorted == False:
             break
 print(arr)
+
+
+# Selection Sort Implementation
+# To implement the Selection Sort algorithm in a programming language, we need:
+
+# An array with values to sort.
+# An inner loop that goes through the array, finds the lowest value, and moves it to the front of the array. This loop must loop through one less value each time it runs.
+# An outer loop that controls how many times the inner loop must run. For an array with 
+# n
+#  values, this outer loop must run 
+# n
+# −
+# 1
+#  times.
+
+my_array = [64, 34, 25, 5, 22, 11, 90, 12]
+
+n = len(my_array)
+
+for i in range (n-1):
+    min_val = my_array[i]
+    for j in range (i+1, n):
+        if my_array[j] < min_val:
+            min_val = my_array[j]
+    # print(min_val)
+    my_array.remove(min_val)
+    my_array.insert(i, min_val)
+
+print(my_array)
