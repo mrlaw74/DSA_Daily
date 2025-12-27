d7e113051fa986b7d914916db6fa85c290895be4
NVLuat <129598838+mrlaw74@users.noreply.github.com>
2025-12-27 07:06:12 +0000

    update other silution for butble sord

--- array.py
+++ array.py
@@ -24,3 +24,15 @@ for i in range(n-1):
             my_arr[j], my_arr[j+1] = my_arr[j+1], my_arr[j]
 
 print(my_arr)
+
+arr = [1, 4, 2, 9, 3, 7]
+
+n = len(arr)
+
+for i in range (n-1):
+    for j in range (n-i-1):
+        if arr[j] > arr[j+1]:
+            temp = arr[j]
+            arr[j] = arr[j+1]
+            arr[j+1] = temp
+print(arr)
