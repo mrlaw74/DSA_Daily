7f479a559fa4f795f0e16922975699582fd45669
NVLuat <129598838+mrlaw74@users.noreply.github.com>
2025-12-27 07:10:18 +0000

    improve sort

--- array.py
+++ array.py
@@ -30,9 +30,13 @@ arr = [1, 4, 2, 9, 3, 7]
 n = len(arr)
 
 for i in range (n-1):
+    sorted = False
     for j in range (n-i-1):
         if arr[j] > arr[j+1]:
             temp = arr[j]
             arr[j] = arr[j+1]
             arr[j+1] = temp
+            sorted = True
+    if sorted == False:
+            break
 print(arr)
